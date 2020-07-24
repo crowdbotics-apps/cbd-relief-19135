abf4989964adfab07eaa9410ed1eb7b11c26728f
Crowdbotics <team@crowdbotics.com>
2020-07-24 03:46:40 +0000

    Initial commit.

---
+++ android/app/src/main/java/com/cbd_relief_19135/MainActivity.java
@@ -0,0 +1,15 @@
+package com.cbd_relief_19135;
+
+import com.facebook.react.ReactActivity;
+
+public class MainActivity extends ReactActivity {
+
+    /**
+     * Returns the name of the main component registered from JavaScript.
+     * This is used to schedule rendering of the component.
+     */
+    @Override
+    protected String getMainComponentName() {
+        return "cbd_relief_19135";
+    }
+}
